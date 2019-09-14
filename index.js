498f4cb08add53ef51432e1cc89c565d0e103b64
AngelLegion <luisangelmf@gmail.com>
2019-09-14 01:37:51 -0500

    cambiando todo en el servidor

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const socketIo = require('socket.io');
 const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-//importar base de datos
+const sockete = require(path.join(__dirname, 'sockete'))
 const bdquestions = require(path.join(__dirname, 'questions'))
 const bdanswers = require(path.join(__dirname, 'answers'))
-
 const app = express();
 
 //webpack
@@ -29,88 +28,41 @@ server.listen(app.get('port'), ()=>{
     console.log("Server Running", app.get('port'));
 })
 
-//game
-var userAdmin = 'admin';
-var passAdmin = '1234';
-var gameBegin = false;
-var vgameOver = false;
-var juegoId = 0;
-var ttime = 3; //cambiar a 60
-var qtime = ttime;
-var numPreg = 0;
-var liberarDetalle = false;
-const totpreg = bdquestions.length;
-var users = {};
-var respuestas = {};
-var respuestasOk = {};
-
 //socket
+
+//variables de estado
+var estadoJuego = {
+    gameBegin: false,
+    totalTime: 3, //segundos
+    gameInitial: 0,
+    gameRest: 0,
+    gameEnd: false,
+    juegoId: 0,
+    liberarDetalle: false,
+    totpreg: bdquestions.length,
+    numUsers: 1,
+    estadisticas: [],
+    userAdmin: 'admin',
+    passAdmin: '1234',
+    usuarios: {
+    }
+}
 io.on('connection', socket => {
     console.log('socket connected: ', socket.id);
-    socket.emit('myId', socket.id);
-    socket.on('beginGame', user => {
-        user['user'] = user['user'].toLowerCase();
-        if (user['user'] == userAdmin && user['pass'] == passAdmin) {
-            console.log("let's the games begin");
-            gameBegin = true;
-            io.sockets.emit('beginGame', gameBegin);
-            //ir emitiendo las preguntas
-            juegoId = setInterval(juegoPreguntas, 1000);
-        }else {
-            socket.emit('beginGame', gameBegin);
-        }
-    });
-    socket.on('amIAdmin', user => {
-        user['user'] = user['user'].toLowerCase();
-        if(user['user'] == userAdmin && user['pass'] == passAdmin){
-            socket.emit('ImAdmin', '');
-        }
-    });
-    socket.on('reboot', user => {
-        user['user'] = user['user'].toLowerCase();
-        if (user['user'] == userAdmin && user['pass'] == passAdmin) {
-            limpiarJuego();
-        }
-    });
-    socket.on('isGameBegin', () => {
-        socket.emit('beginGame', gameBegin);
-    });
-    socket.on('isGameOver', () => {
-        if(vgameOver){
-            io.sockets.emit('gameOver', '');
-        }
-    });
-    socket.on('answer', answer => {
-        answer['id'] = answer['id'].toLowerCase();
-        console.log('Answer: ', answer)
-        if ( !respuestas.hasOwnProperty(answer['id']) ) {
-            respuestas[answer['id']] = {};
-            respuestasOk[answer['id']] = [];
-        }
-        respuestas[answer['id']][answer['q']]=answer['ans'];
-        checkAnswer(answer)
-        console.log('respOk: ', respuestasOk)
-    });
     socket.on('newUser', user =>{
         user['user'] = user['user'].toLowerCase();
         if(user.hasOwnProperty('user') && user.hasOwnProperty('pass') && user['user'] !== '' && user['pass'] !== ''){
-            var ok = users.hasOwnProperty(user['user']);
+            var ok = estadoJuego.usuarios.hasOwnProperty(user['user']);
             if(!ok){
                 //administrador
-                if(user['user'] == userAdmin && user['pass'] == passAdmin){
-                    users[user['user']] = user['pass'];
-                    respuestas[user['user']] = {};
-                    respuestasOk[user['user']] = [];
-                    io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
+                if(user['user'] === estadoJuego.userAdmin && estadoJuego.passAdmin === user['pass']){
+                    agregarUsuario(user, socket.id);
                     socket.emit('ImAdmin', '');
                 }else if (user['user'] == userAdmin){
                     console.log("contra incorrecta admin")
                     socket.emit('userNotValid', '');
                 }else {
-                    users[user['user']] = user['pass'];
-                    respuestas[user['user']] = {};
-                    respuestasOk[user['user']] = [];
-                    io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
+                    agregarUsuario(user, socket.id);
                 }
             }else{
                 checkUser(user, socket);
@@ -119,147 +71,61 @@ io.on('connection', socket => {
     });
     socket.on('deleteUser', user => {
         user['user'] = user['user'].toLowerCase();
-        var ok = users.hasOwnProperty(user['user']);
+        var ok = estadoJuego.usuarios.hasOwnProperty(user['user']);
         if (ok) {
-            if(users[user['user']] == user['pass']) {
-                delete users[user['user']]
-                delete respuestas[user['user']];
-                delete respuestasOk[user['user']];
-                io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
+            if(estadoJuego.usuarios[user['user']]['pass'] == user['pass']) {
+                delete estadoJuego.usuarios[user['user']]
             }
         }
     });
     socket.on('checkUser', user => {
         checkUser(user, socket);
     })
-    socket.on("whichQuestion", ()=>{
-        var question = emitQuestion();
-        io.sockets.emit('question', question)
-    })
-    socket.on('liberarDetalle', user => {
+    socket.on('iniciarCuestionario', user =>{
         user['user'] = user['user'].toLowerCase();
-        if(user['user'].toLowerCase() == userAdmin && user['pass'] == passAdmin){
-            liberarDetalle = true;
-            io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
-            io.sockets.emit('allQuestion', bdquestions);
-            io.sockets.emit('adminCorrectAns', bdanswers);
-            io.sockets.emit('detalleLiberado', liberarDetalle)
+        var ok = estadoJuego.usuarios.hasOwnProperty(user['user']);
+        if (ok && user['user'] === estadoJuego.userAdmin && estadoJuego.passAdmin === user['pass']){
+            //inicializar contador, set tiempo de inicio e ir revisando cada 10 segundos hasta que se termine el tiempo
         }
     })
-
+    //socket.on contestePregunta, ver si hay tiempo para registrar su respuesta, registrar y regresarle actualizado su estado
 })//finaliza socket
 
-//funciones adicionales para juego
+function agregarUsuario(user, id){
+    estadoJuego.usuarios[user['user']] = {};
+    estadoJuego.usuarios[user['user']]['pass'] = user['pass'];
+    estadoJuego.usuarios[user['user']]['id'] = id;
+    estadoJuego.usuarios[user['user']]['respuestas'] = {};
+    estadoJuego.usuarios[user['user']]['puntajePP'] = [];
+    estadoJuego.usuarios[user['user']]['puntajeTotalUser'] = [];
+    estadoJuego.usuarios[user['user']]['termino'] = false;
+    //enviarle al usuario el estado actual del juego
+}
 
 function checkUser(user, socket) {
     user['user'] = user['user'].toLowerCase();
-    var ok = users.hasOwnProperty(user['user']);
+    var ok = estadoJuego.usuarios.hasOwnProperty(user['user']);
         if (ok) {
-            if(users[user['user']] == user['pass']) {
-                if(user['user'].toLowerCase() == userAdmin && user['pass'] == passAdmin){
+            if(estadoJuego.usuarios[user['user']]['pass'] == user['pass']) {
+                //si es el administrador
+                if(user['user'] === estadoJuego.userAdmin && estadoJuego.passAdmin === user['pass']){
                     socket.emit('ImAdmin', '');
-                    socket.emit('allQuestion', bdquestions);
-                    //socket.emit('resultados', {ans: respuestas, res: respuestasOk});
+                    socket.emit('usuarios', estadoJuego.usuarios);
                     socket.emit('adminCorrectAns', bdanswers);
-                    socket.emit('passwords', users);
-                    if(liberarDetalle){
-                        socket.emit('detalleLiberado', liberarDetalle);
-                    }
-                }//cambiar adminCorrectAns para que solo admin vea respuestas hasta que las libere
-                else{
-                    //para poder implementar resultados personales debo cambiar las estadisticas
-                    //socket.emit('resultados', {ans: respuestas[user['user']], res: respuestasOk[user['user']]});
+                    //cambiar como se reciben las passwords en el front y los resultados ahora todo va en usuarios
+                }else{
                     if(liberarDetalle){
                         socket.emit('adminCorrectAns', bdanswers);
                     }
+                    socket.emit('misResultados', estadoJuego.usuarios[user['user']]);
                 }
                 socket.emit('allQuestion', bdquestions);
-                socket.emit('resultados', {ans: respuestas, res: respuestasOk});
-                socket.emit('detalleLiberado', liberarDetalle)
+                socket.emit('estadisticas', estadoJuego.estadisticas);
+                socket.emit('detalleLiberado', estadoJuego.liberarDetalle)
             }else{
                 socket.emit('userNotValid', '');
             }
         }else{
             socket.emit('userNotValid', '');
         }
-}
-
-function checkAnswer(answer) {
-    var isequal = answer['q'] - respuestasOk[answer['id']].length;
-    if (isequal !== 0) {
-        //si se envio información doble
-        if(isequal < 0) {
-            return;
-        }
-        for(let i = 0; i < isequal; i++){
-            respuestasOk[answer['id']].push(0);
-        }
-    }
-    if (answer['ans'] === bdanswers[answer['q']]){
-        respuestasOk[answer['id']].push(1);
-    }
-    else {
-        respuestasOk[answer['id']].push(0);
-    }
-}
-function checkCompleteAns(npregActual){
-    for(var key in respuestasOk){
-        let isequal = npregActual - respuestasOk[key].length;
-        if (isequal !== 0) {
-            for(let i = 0; i < isequal; i++){
-                respuestasOk[key].push(0);
-            }
-        }
-    }
-    io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
-}
-
-function juegoPreguntas(){
-    if(qtime == ttime){
-        var question = emitQuestion();
-        io.sockets.emit('question', question)
-        checkCompleteAns(numPreg);
-    }
-    io.sockets.emit('time', qtime);
-    if(--qtime == 0){
-        qtime = ttime;
-        numPreg++;
-    }
-    if (numPreg >= totpreg) {
-        checkCompleteAns(numPreg);
-        gameOver();
-        return;
-    }
-    
-}
-
-function emitQuestion(){
-    //emitir pregunta a frontend
-    var question = {
-        nq: numPreg,
-        ...bdquestions[numPreg]
-    }
-    return question;
-}
-
-function gameOver(){
-    clearInterval(juegoId);
-    vgameOver = true;
-    io.sockets.emit('gameOver', '');
-    console.log(respuestas);
-    console.log(respuestasOk);
-}
-
-function limpiarJuego(){
-    clearInterval(juegoId);
-    gameBegin = false;
-    vgameOver = false;
-    juegoId = 0;
-    numPreg = 0;
-    qtime = ttime;
-    users = {};
-    respuestas = {};
-    respuestasOk = {};
-    io.sockets.emit('reboot', '');
-    io.sockets.emit('resultados', {ans: respuestas, res: respuestasOk});
 }
